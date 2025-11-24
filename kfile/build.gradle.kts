f9fd7380de4d8c62525c6147bcda88634cf29da2
Julius Vincent Babies <julius@familie-babies.de>
2025-11-24 21:58:10 +0100

    chore: Library setup

--- kfile/build.gradle.kts
+++ kfile/build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.symbol.processing)
     alias(libs.plugins.kotest)
+
+    alias(libs.plugins.maven.publish)
 }
 
-group = "es.jvbabi"
-version = "1.0-SNAPSHOT"
+group = "io.github.julius-babies"
+version = System.getenv("VERSION")?.ifBlank { null } ?: "unspecified"
 
 repositories {
     mavenCentral()
@@ -37,4 +39,42 @@ kotlin {
 tasks.withType<Test>().configureEach {
     logger.lifecycle("UP-TO-DATE check for $name is disabled, forcing it to run.")
     outputs.upToDateWhen { false }
+}
+
+tasks.withType<PublishToMavenRepository> {
+    dependsOn(tasks.withType<Test>())
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    if (!gradle.startParameter.taskNames.any { it.contains("publishToMavenLocal") }) {
+        signAllPublications()
+    }
+    coordinates(project.group.toString(), project.name, project.version.toString())
+
+    pom {
+        name = "kfile"
+        description = "A Kotlin/Native + JVM Library for file operations"
+        url = "https://github.com/Julius-Babies/KFile"
+
+        developers {
+            developer {
+                id = "julius-vincent-babies"
+                name = "Julius Vincent Babies"
+                email = "julvin.babies@gmail.com"
+                url = "https://github.com/Julius-Babies"
+            }
+        }
+
+        scm {
+            url = "https://github.com/Julius-Babies/KFile"
+        }
+
+        licenses {
+            license {
+                name = "The MIT License (MIT)"
+                url = "https://opensource.org/license/MIT"
+            }
+        }
+    }
 }
